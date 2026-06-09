d1b5a32f85b144d722d1f0771c9ee11656226c4b
hosansohag147-arch <hosansohag147@gmail.com>
2026-06-09 20:38:27 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -451,7 +451,7 @@ export default function App() {
      top: '-300x',
      left: '0',
      width: '100%',
-     height: 'calc(100% + 750px)',
+     height: 'calc(100% + 850px)',
      pointerEvents: 'auto'
    }}
                  allowFullScreen
