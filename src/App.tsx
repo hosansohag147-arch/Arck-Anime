87962bb2d549d266972df9ac2c4a2364d093b969
hosansohag147-arch <hosansohag147@gmail.com>
2026-06-09 20:14:59 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -448,7 +448,7 @@ export default function App() {
    className="w-full border-none bg-black animate-in fade-in duration-500"
    style={{
      position: 'absolute',
-     top: '-550px',
+     top: '-300px',
      left: '0',
      width: '100%',
      height: 'calc(100% + 750px)',
